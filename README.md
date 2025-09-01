6c28d98f2122c85aa7289ba5144592bb7b5c1de8
johani23 <ahmad.a.johani@gmail.com>
2025-09-01 13:40:42 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clusterverse Frontend
+
+React project.
---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const App = () => <h1>Clusterverse Frontend Ready</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
