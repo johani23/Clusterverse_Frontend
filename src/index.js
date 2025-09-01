6146db11f4f2ffb97d75b3250ec7561bc402720a
johani23 <ahmad.a.johani@gmail.com>
2025-09-01 14:31:29 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
-const App = () => <h1>Clusterverse Frontend Ready</h1>;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
